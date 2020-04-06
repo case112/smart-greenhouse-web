97a8d803726086c76f08303c7d047343163be4f4
Oskar <case112@users.noreply.github.com>
2020-04-06 19:31:14 +0300

    Added sensor name to dht22 model

--- data/models.py
+++ data/models.py
@@ -6,3 +6,6 @@ class Dht22(models.Model):
     temperature = models.CharField(max_length=5)
     humidity = models.CharField(max_length=5)
     date = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.sensor
