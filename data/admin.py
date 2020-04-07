c63377c4512113940f5ee27421645efe02a5526a
Oskar <case112@users.noreply.github.com>
2020-04-07 14:27:56 +0300

    Created new model Chirp

--- data/admin.py
+++ data/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from . models import Dht22
+from . models import Dht22, Chrip
 
 class Dht22Admin(admin.ModelAdmin):
     list_display = ('sensor', 'temperature', 'humidity', 'date')
     list_filter = ('date',)
 
+class ChirpAdmin(admin.ModelAdmin):
+    list_display = ('sensor', 'moisture_value', 'moisture', 'temperature', 'date')
+    list_filter = ('date',)
+
 admin.site.register(Dht22, Dht22Admin)
+admin.site.register(Chrip, ChirpAdmin)
+
--- data/models.py
+++ data/models.py
@@ -13,3 +13,14 @@ class Dht22(models.Model):
     @property
     def dht22_id(self):
         return self.id
+
+
+class Chrip(models.Model):
+    sensor = models.CharField(max_length=20)
+    moisture_value = models.CharField(max_length=3)
+    moisture = models.CharField(max_length=5)
+    temperature = models.CharField(max_length=5)
+    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.sensor
