c7d4c004246d12b492dc92c92fcd3e1211a2678b
Oskar <case112@users.noreply.github.com>
2020-04-11 14:41:10 +0300

    Added new logic for displaying charts from database

--- data/views.py
+++ data/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+import datetime
+from django.http import JsonResponse
+from . models import Dht22
+
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+
+def temperature_chart(request):
+    dates = []
+    data = []
+
+    date_from = datetime.datetime.now() - datetime.timedelta(days=1)
+
+    queryset = Dht22.objects.filter(date__gte=date_from, sensor__exact='Greenhouse2')[:16]
+    for entry in queryset:
+        x = str(entry.date)
+        dates.append(x[11:-3])
+        data.append(entry.temperature)
+    
+    return JsonResponse(data={
+        'dates': dates,
+        'data': data,
+    })
--- greenhouse/urls.py
+++ greenhouse/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from data.views import index
+from data.views import index, temperature_chart
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('temperature-chart/', temperature_chart, name='temperature-chart'),
 ]
 
