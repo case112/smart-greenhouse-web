820ff07b4673ed6815a0929851fe306b233636b1
Oskar <case112@users.noreply.github.com>
2020-06-12 22:07:40 +0300

    Getting new models to display data

--- data/views.py
+++ data/views.py
@@ -6,9 +6,9 @@ from . models import State, Sensor, Object
 
 def index(request):
     latest = Sensor.objects.all().order_by('-created_at')[:10]
-    window_state = State.objects.all().latest('created_at')
-    voltage = State.objects.all().latest('created_at')
-    charge = State.objects.all().latest('created_at')
+    window_state = State.objects.filter(state_name=2).order_by('created_at')
+    voltage = State.objects.filter(state_name=1).order_by('created_at')
+    charge = State.objects.filter(state_name=3).order_by('created_at')
     context = {
 		'latest': latest,
         'window_state': window_state,
@@ -18,28 +18,28 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-# def temp_today(request):
-#     time = []
-#     data = []
-#     queryset = Dht22.objects.filter(sensor__exact='Greenhouse2').order_by('-date')[:16][::-1]
-#     for entry in queryset:
-#         x = str(entry.date)
-#         time.append(x[11:-3])
-#         data.append(entry.temperature)
-#     return JsonResponse(data={
-#         'time': time,
-#         'data': data,
-#     })
+def temp_today(request):
+    time = []
+    data = []
+    queryset = Sensor.objects.filter(sensor_name=1).order_by('-created_at')[:16][::-1]
+    for entry in queryset:
+        x = str(entry.created_at)
+        time.append(x[11:-3])
+        data.append(entry.temperature)
+    return JsonResponse(data={
+        'time': time,
+        'data': data,
+    })
 
-# def hum_today(request):
-#     time = []
-#     data = []
-#     queryset = Dht22.objects.filter(sensor__exact='Greenhouse2').order_by('-date')[:16][::-1]
-#     for entry in queryset:
-#         x = str(entry.date)
-#         time.append(x[11:-3])
-#         data.append(entry.humidity)
-#     return JsonResponse(data={
-#         'time': time,
-#         'data': data,
-#     })
+def hum_today(request):
+    time = []
+    data = []
+    queryset = Sensor.objects.filter(sensor_name=1).order_by('-created_at')[:16][::-1]
+    for entry in queryset:
+        x = str(entry.created_at)
+        time.append(x[11:-3])
+        data.append(entry.humidity)
+    return JsonResponse(data={
+        'time': time,
+        'data': data,
+    })
